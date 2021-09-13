f6327b9d2133275ee92d31b80996f2480e95a033
krgarima <kumargarima59@gmail.com>
2021-09-13 23:33:18 +0530

    code

--- index.js
+++ index.js
@@ -2,65 +2,71 @@ const inpDate = document.querySelector('#inpDate');
 const btndate = document.querySelector('#btn');
 const remDays = document.querySelector('#message');
 
-function checkDate()
-{
-    const newDate = (inpDate.value).replaceAll("-","");
-    const splitNumber = newDate.split('');
-    const reversedNumber = reverseNum(splitNumber);
+function checkDate() {
+    const oldDate = (inpDate.value).replaceAll("-", "");
+    const splitNumber = oldDate.split('');
 
-    //console.log(newDate);
-    // const joinNumber = reversedNumber.join('');
-    // console.log(joinNumber);
+    const oldNumber = originalNumber(splitNumber);
+    const reversedNumber = reverseNum(splitNumber);
+    if (oldNumber === reversedNumber) {
+        remDays.innerText = "Birthday is a Palindrome."
+    } else {
+        difference(oldNumber, reversedNumber);
+        remDays.innerText = "Birthday is a NOT Palindrome."
+    }
 }
 
-function reverseNum(num)
-{
-    if(num[6]===num[3])
+function originalNumber(num) {
+    let i = num[6] + num[7] + num[4] + num[5] + num[0] + num[1] + num[2] + num[3];
+    return i;
+}
+
+function reverseNum(num) {
+    let j = num[3] + num[2] + num[1] + num[0] + num[5] + num[4] + num[7] + num[6];
+    return j;
+}
+
+function difference(a, b) {
+    let i=0;
+    const date=a[0]+a[1]; const month=a[2]+a[3]; const year=a[4]+a[5]+a[6]+a[7];
+    for(i=year;i<=9999;i++)
     {
-        if(num[7]===num[2])
-        {
-            if(num[4]===num[1])
-            {
-                if(num[5]===num[0])
-                console.log("yes");
-                else
-                console.log("no");
-            }
-        }
+        if(d<=31)
+        const diff = 
     }
+}
 
-
-    // let i,j=7;
-    // var rev="";
-    // //let number = Number(num);
-    // //console.log(num);
-    // for(i=0;i<4;i++)
-    // {
-    //     if(num[i]===num[j])
-    //     {console.log(num[i] +"="+ num[j]);}
-    //     else
-    //     {i=5;console.log("no");}
-    //     j--;
-    // }
-   
-}   
-//     // console.log(num);
-
-//     // let l=num.length;
-//     // let i,j;
-//     // j=l-1;
-//     // //let number = Number(num);
-//     // for(i=0;i<l;i++)
-//     // {
-//     //         // if( num.charAt(i) === num.charAt(j) )          
-//     //         //     {j--;console.log(num)}
-//     //         //     else
-//     //         //     console.log("Nooo");
-//     //         console.log("At "+ i+" = "+num.charAt(i));
-
+// if(num[6]===num[3])
+// {
+//     if(num[7]===num[2])
+//     {
+//         if(num[4]===num[1])
+//         {
+//             if(num[5]===num[0])
+//             console.log("Yes! Entered birthday is a palindrome number");
+//             else
+//             findDifference(num);
+//         }
+//         else
+//         findDifference(num);
 //     }
-
-
+//     else
+//     findDifference(num);
 // }
+// else
+// findDifference(num);
+
+//  function findDifference(num)
+//  {
+//     let year=num[0]+num[1]+num[2]+num[3];
+//     let month=num[4]+num[5];
+//     let date=num[6]+num[7];
+//     let i,result=0;
+//     for(i=year ; i<9999;i++)
+//     {
+//         if((date<=31)&&(month<12))
+//         result
+//     }
+//  }   
 
 btndate.addEventListener("click", checkDate);
