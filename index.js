70495e0370afac3d79f753d0cb4a139eee784ee6
krgarima <kumargarima59@gmail.com>
2021-09-20 17:59:26 +0530

    updated index.js

--- index.js
+++ index.js
@@ -3,66 +3,66 @@ const btndate = document.querySelector('#btn');
 const remDays = document.querySelector('#message');
 
 function checkDate() {
-    if(inpDate===" ")
-    remDays.innerText = "Field is empty. Enter a valid date."
-    const oldDate = (inpDate.value).replaceAll("-", "");
-    const splitNumber = oldDate.split('');
 
-    const oldNumber = originalNumber(splitNumber);
-    const reversedNumber = reverseNum(splitNumber);
-    
-    remDays.innerText = "Please wait...";
-    if (oldNumber === reversedNumber) {
-        remDays.innerText = "Your birthday is a Palindrome."
-    } else {
-        nextPalindromeNumber(oldNumber, reversedNumber);
-        remDays.innerText = "Sorry! your birthday is NOT a Palindrome."
-    }
+    if ((inpDate.value)) {
+        const oldDate = (inpDate.value).replaceAll("-", "");
+        const splitNumber = oldDate.split('');
+
+        const oldNumber = originalNumber(splitNumber);
+        const reversedNumber = reverseNum(splitNumber);
+
+        if (Number(oldNumber) === Number(reversedNumber)) {
+            remDays.innerText = "Your birthday is a Palindrome."
+        } else {
+            //nextPalindromeNumber(oldNumber, reversedNumber);
+            remDays.innerText = "Sorry! your birthday is NOT a Palindrome."
+        }
+    } else
+        remDays.innerText = "Error: empty field!";
 }
 
 function originalNumber(num) {
-    let i = num[6] + num[7] + num[4] + num[5] + num[0] + num[1] + num[2] + num[3];
+    const i = num[6] + num[7] + num[4] + num[5] + num[0] + num[1] + num[2] + num[3];
     return i;
 }
 
 function reverseNum(num) {
-    let j = num[3] + num[2] + num[1] + num[0] + num[5] + num[4] + num[7] + num[6];
+    const j = num[3] + num[2] + num[1] + num[0] + num[5] + num[4] + num[7] + num[6];
     return j;
 }
 
-function checkPalindrome(a,b)
-{
-    if(originalNumber(a)===reverseNum(b))
-    return
-    remDays.innerText = a + " is Palindrome of "+ b;
+function checkPalindrome(a, b) {
+    if (originalNumber(a) === reverseNum(b))
+        return
+    remDays.innerText = a + " is Palindrome of " + b;
 }
 
-function nextPalindromeNumber(a, b) {
-    let i,j,k;
-    const date=Number(a[0]+a[1]); const month=Number(a[2]+a[3]); const year=Number(a[4]+a[5]+a[6]+a[7]);
-    
-    const original = date + month + year;
-    let d = Number(date);
-    for(i=year;i<=5000;i++)
-    {
-        for(j=month;j<=12;j++)
-        {
-            for(k=d;k<=31;k++)
-            {
-                console.log(d+" ");
-                original.toString = k.toString + j.toString + i.toString;
-                reverse = reverseNum(original);
-                console.log(original+ " "+reverse);
-                if(original===reverse)
-                remDays.innerText = reverse + "is next Palindrome of "+date;
-                else
-                remDays.innerText = "couldn't find";
-            }
-            k=1;
-        }
-        j=1;
-    }
-    
-}  
+// function nextPalindromeNumber(a, b) {
+//     let i,j,k;
+//     const date=Number(a[0]+a[1]); const month=Number(a[2]+a[3]); const year=Number(a[4]+a[5]+a[6]+a[7]);
+
+//     const original = date + month + year;
+//     let d = Number(date);
+//     for(i=year;i<=5000;i++)
+//     {
+//         for(j=month;j<=12;j++)
+//         {
+//             for(k=d;k<=31;k++)
+//             {
+//                 console.log(d+" ");
+//                 original.toString = k.toString + j.toString + i.toString;
+//                 reverse = reverseNum(original);
+//                 console.log(original+ " "+reverse);
+//                 if(original===reverse)
+//                 remDays.innerText = reverse + "is next Palindrome of "+date;
+//                 else
+//                 remDays.innerText = "couldn't find";
+//             }
+//             k=1;
+//         }
+//         j=1;
+//     }
+
+// }  
 
 btndate.addEventListener("click", checkDate);
